9d56c6b659049a885b65fbfb774a1a5186726571
Robert Norwood <72119590+RobertNorwood2022@users.noreply.github.com>
2020-11-16 19:41:40 -0600

    started on task 2

--- exact.m
+++ exact.m
@@ -3,37 +3,19 @@
 %set enviornment
 clc
 clear
-format long
+format long 
+format compact
 
 
 %define the given variables
+global A
+global B
 A = [196.5 1.5 -199; 200.5 -2.5 -199; 396.5 1.5 -399];
 B = [.5 .5 -2; 2.5 -1.5 -2; 3.5 .5 -5];
 Xa0 = [1 2 3]';
 Xb0 = [1 2 3]';
 
 
-%solve using built in dsolve to check work
-fprintf('Computed solutions using dsolve: \n');
-syms x(t) y(t) z(t)
-X = [x;y;z];
-odesA =diff(X) == A*X;
-cond = X(0) == Xa0;
-[xsolA(t), ysolA(t), zsolA(t)] = dsolve(odesA,cond);
-
-
-syms x(t) y(t) z(t)
-X = [x;y;z];
-odesB = B*X;
-cond = X(0) == Xa0;
-[xsolB(t), ysolB(t), zsolB(t)] = dsolve(odesA,cond);
-
-%find when t=2
-fprintf('t=2 for equation 1:')
-[double(xsolA(2));double(ysolA(2));double(zsolA(2))]
-
-fprintf('t=2 for equation 2:')
-[double(xsolB(2));double(ysolB(2));double(zsolB(2))]
 
 %calculate eigenvalue and eigenvectors 
 format short
@@ -61,9 +43,9 @@ c1 = c(1);
 c2 = c(2);
 c3 = c(3);
 format long
-xA = c1*(scaled_evec_A(1,1) * exp(evalA(1) * t)) + c2*(scaled_evec_A(1,2) * exp(evalA(2) * t)) + c3*(scaled_evec_A(1,2) * exp(evalA(3) * t));
-yA = c1*(scaled_evec_A(2,1) * exp(evalA(1) * t)) + c2*(scaled_evec_A(2,2) * exp(evalA(2) * t)) + c3*(scaled_evec_A(2,2) * exp(evalA(3) * t));
-zA = c1*(scaled_evec_A(3,1) * exp(evalA(1) * t)) + c2*(scaled_evec_A(3,2) * exp(evalA(2) * t)) + c3*(scaled_evec_A(3,2) * exp(evalA(3) * t));
+xA = c1*(scaled_evec_A(1,1) * exp(evalA(1) * t)) + c2*(scaled_evec_A(1,2) * exp(evalA(2) * t)) + c3*(scaled_evec_A(1,3) * exp(evalA(3) * t));
+yA = c1*(scaled_evec_A(2,1) * exp(evalA(1) * t)) + c2*(scaled_evec_A(2,2) * exp(evalA(2) * t)) + c3*(scaled_evec_A(2,3) * exp(evalA(3) * t));
+zA = c1*(scaled_evec_A(3,1) * exp(evalA(1) * t)) + c2*(scaled_evec_A(3,2) * exp(evalA(2) * t)) + c3*(scaled_evec_A(3,3) * exp(evalA(3) * t));
 X = [xA yA zA]'
 
 %solutions at t=2 for B
@@ -73,7 +55,7 @@ c1 = c(1);
 c2 = c(2);
 c3 = c(3);
 format long
-xB = c1*(scaled_evec_B(1,1) * exp(evalB(1) * t)) + c2*(scaled_evec_B(1,2) * exp(evalB(2) * t)) + c3*(scaled_evec_B(1,2) * exp(evalB(3) * t));
-yB = c1*(scaled_evec_B(2,1) * exp(evalB(1) * t)) + c2*(scaled_evec_B(2,2) * exp(evalB(2) * t)) + c3*(scaled_evec_B(2,2) * exp(evalB(3) * t));
-zB = c1*(scaled_evec_B(3,1) * exp(evalB(1) * t)) + c2*(scaled_evec_B(3,2) * exp(evalB(2) * t)) + c3*(scaled_evec_B(3,2) * exp(evalB(3) * t));
+xB = c1*(scaled_evec_B(1,1) * exp(evalB(1) * t)) + c2*(scaled_evec_B(1,2) * exp(evalB(2) * t)) + c3*(scaled_evec_B(1,3) * exp(evalB(3) * t));
+yB = c1*(scaled_evec_B(2,1) * exp(evalB(1) * t)) + c2*(scaled_evec_B(2,2) * exp(evalB(2) * t)) + c3*(scaled_evec_B(2,3) * exp(evalB(3) * t));
+zB = c1*(scaled_evec_B(3,1) * exp(evalB(1) * t)) + c2*(scaled_evec_B(3,2) * exp(evalB(2) * t)) + c3*(scaled_evec_B(3,3) * exp(evalB(3) * t));
 X = [xB yB zB]'
---
+++ prime.m
@@ -0,0 +1,7 @@
+function f = prime(t,X)
+global count
+global matrix
+count  = count + 1;
+f = matrix*X;
+end
+
