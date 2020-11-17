d040c2eaa87b98d6eb4af7ca812ffdfb589ce9f2
Robert Norwood <72119590+RobertNorwood2022@users.noreply.github.com>
2020-11-16 19:50:21 -0600

    Finished ode15sstiff

--- ode15sstiff.m
+++ ode15sstiff.m
@@ -13,27 +13,30 @@ Xa0 = [1 2 3];
 Xb0 = [1 2 3];
 tfin = 2;
 t = [0 tfin];
-relative_tolerance_1 = 1.75e-4;
-relative_tolerance_2 = 1.5e-4;
+relative_tolerance_1 = 5e-6;
+relative_tolerance_2 = 1.5e-5;
 
 %solution for A
 count = 0;
 matrix = A;
 options = odeset('RelTol',relative_tolerance_1);
-[t,XsolA] = ode45(@prime,t,Xa0, options);
-xa = XsolA(:,1);
+[t,XsolAs] = ode15s(@prime,t,Xa0, options);
+xas = XsolAs(:,1);
 fprintf('--Equation 1-- \n');
 fprintf('Count: %i \n', count);
-fprintf('nsteps: %i \n', length(xa));
-fprintf('Final x: %10.10f \n', xa(length(xa)));
+fprintf('nsteps: %i \n', length(xas));
+fprintf('Final x: %10.10f \n', xas(length(xas)));
 
 %solution for B
 count = 0;
 martix = B;
 options = odeset('RelTol',relative_tolerance_2);
-[t, XsolB] = ode45(@prime,t,Xb0, options);
-xb = XsolB(:,1);
+[t, XsolBs] = ode15s(@prime,t,Xb0, options);
+xbs = XsolBs(:,1);
 fprintf('--Equation 2-- \n');
 fprintf('Count: %i \n', count);
-fprintf('nsteps: %i \n', length(xb));
-fprintf('Final x: %10.10f \n', xb(length(xb)));
+fprintf('nsteps: %i \n', length(xbs));
+fprintf('Final x: %10.10f \n', xbs(length(xbs)));
+
+
+%graphing -- make sure to run ode45stiff FIRST
